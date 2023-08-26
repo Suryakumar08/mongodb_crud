77ed87692d3b00cccec9a4a133bbc4e2e2abea11
Suryakumar S <suryakumar131419@gmail.com>
2023-08-26 10:16:51 +0530

    initial Commit

---
+++ url.js
@@ -0,0 +1,5 @@
+//module.exports = `mongodb://localhost:27017`
+/*const encodedPassword = encodeURIComponent("131419@sS");
+module.exports = `mongodb+srv://admin:${encodedPassword}@cluster0.mavuja6.mongodb.net/`*/
+//module.exports = `mongodb+srv://admin:131419@sS@cluster0.mavuja6.mongodb.net/`
+module.exports = `mongodb+srv://admin:admin@cluster0.mavuja6.mongodb.net/`
